e54ea60e2308fc044ffa5f9c272bee40c26b6387
Jaysigx <contactjaskaran@gmail.com>
2025-07-05 11:47:48 -0400

    kj

--- postcss.congfig.js
+++
@@ -1,8 +0,0 @@
-import autoprefixer from 'autoprefixer';
-
-// postcss.config.js
-import tailwindcss from '@tailwindcss/postcss';
-
-export default {
-  plugins: [tailwindcss(), autoprefixer()],
-};
